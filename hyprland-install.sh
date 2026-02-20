9b897e32771d57a13089c74be8f87b951ce9b6ee
HAL 9000 <hal9000@local>
2026-02-20 12:23:33 +0200

    Default scale to 1 (not HiDPI)

--- hyprland-install.sh
+++ hyprland-install.sh
@@ -50,7 +50,7 @@ env = __GLX_VENDOR_LIBRARY_NAME,nvidia
 env = WLR_NO_HARDWARE_CURSORS,1
 
 # === MONITOR ===
-monitor = ,preferred,auto,1.5
+monitor = ,preferred,auto,1
 
 # === STARTUP ===
 exec-once = waybar
